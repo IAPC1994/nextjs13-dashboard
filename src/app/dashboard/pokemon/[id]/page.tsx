e40c356c2d54b688deb58862519d11ccd75356e0
Ivan Panussis <ivan.panussis.c@gmail.com>
2024-01-30 23:45:41 -0300

    feat(web): static generation and revalidate

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -8,6 +8,15 @@ interface Props {
     params: { id: string };
 }
 
+export async function generateStaticParams(){
+
+  const static151Pokemons = Array.from({ length: 151 }).map( ( v, i ) => `${i + 1}`);
+
+  return static151Pokemons.map( id => ({
+    id: id
+  }));
+}
+
 export async function generateMetadata( { params}:Props ): Promise<Metadata> {
   try {
     const { id, name } = await getPokemon( params.id );
@@ -28,7 +37,10 @@ export async function generateMetadata( { params}:Props ): Promise<Metadata> {
 const getPokemon = async( id: string ):Promise<Pokemon> => {
   try {
     const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${ id }`,{
-      cache: 'force-cache' //TODO: Change this
+      cache: 'force-cache', //TODO: Change this
+      next:{
+        revalidate: 60 * 60 * 30 *6
+      }
     }).then( res => res.json());
   
     return pokemon;
