4ba433ccd2200e4510409e4d164d98c5b9853a67
Ivan Panussis <ivan.panussis.c@gmail.com>
2024-01-31 23:25:23 -0300

    feat(web): redux toolkit, provider, store and metadata in dashboard page

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,5 +1,11 @@
 import { SimpleWidget } from '../../components/SimpleWidget';
 
+
+export const metadata = {
+ title: 'Admin Dashboard',
+ description: 'Admin Dashboard of the application',
+};
+
 export default function MainPage() {
   return (
     <div className="text-black">
