7c17ebf24b35526acbcac5c67066486969bcc8b0
Ivan Panussis <ivan.panussis.c@gmail.com>
2024-02-03 00:46:33 -0300

    feat(web): Pokemon reducer for favourites

---
+++ src/store/pokemons/pokemonSlice.ts
@@ -0,0 +1,20 @@
+import { SimplePokemon } from '@/app/pokemons';
+import { createSlice } from '@reduxjs/toolkit'
+
+interface PokemonsState{
+    [key : string]: SimplePokemon,
+}
+
+const initialState:PokemonsState = {
+    '1': { id: '1', name: 'bulbasaur'}
+}
+
+const pokemonSlice = createSlice({
+  name: 'pokemons',
+  initialState,
+  reducers: {}
+});
+
+export const {  } = pokemonSlice.actions;
+
+export default pokemonSlice.reducer;
